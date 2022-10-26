fc31ed2dddf9ddc6853e30a33c6df31bd6746299
jecontreras <joseeduar147@gmail.com>
2022-10-26 16:00:02 -0500

    cambios pred

--- api/models/ArticuloTalla.js
+++ api/models/ArticuloTalla.js
@@ -12,6 +12,9 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    codigo:{
+      type: 'string'
+    },
     articulo:{
       model: 'articulos'
     },
