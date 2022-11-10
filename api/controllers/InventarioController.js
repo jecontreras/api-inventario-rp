e3aaa3c5e153e0afbf12d524ed61e56a7146b1a4
jecontreras68 <joseeduar147@gmail.com>
2022-11-10 15:38:27 -0500

    cambios new

--- api/controllers/InventarioController.js
+++ api/controllers/InventarioController.js
@@ -12,4 +12,21 @@
      resultado = await QuerysServices(Inventario, params);
      return res.ok(resultado);
  }
+
+ Procedures.inventario = async( req, res)=>{
+    let params = req.allParams();
+    let resultado = Object();
+    resultado = await ArticuloColor.find( { where: { estado: 0 } } );
+    for( let row of resultado ){
+        row.listTalla = await Procedures.getArticulos( params.idColor );
+    }
+    return resultado;
+ }
+
+ Procedures.getArticulos = async( data )=>{
+    let resultado = Array();
+    resultado = await ArticuloTalla.find( { where: { estado: 0, listColor: data.id } } ).limit( 1000000 );
+    return resultado;
+ }
+
  module.exports = Procedures;
